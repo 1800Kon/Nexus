2b0a772070c7bfd7223d79443ec13f333dee65bc
Konstantin <55129891+1800Kon@users.noreply.github.com>
2023-11-25 16:37:24 +0100

    Module picker

--- app/components/gridEntry/GridEntry.tsx
+++ app/components/gridEntry/GridEntry.tsx
@@ -4,6 +4,7 @@ import penIcon from '../../../public/penIcon.svg'
 import Image from 'next/image'
 
 const GridEntry: React.FC = () => {
+    const modules = ["Clock", "Tracker", "GitHub", "Crypto ticker", "Tasks", "Book tracker", "Calendar", "Weather", "Calculator", "Notes", "Momentum tracker", "Pomodoro timer", "Links", "Quote of the day", "Countdown", "Alarm"]
     return (
         <><div className='tooltip' data-tip='Click to add a new module'>
             <Image className='cursor-pointer'
@@ -11,16 +12,18 @@ const GridEntry: React.FC = () => {
                 src={penIcon}
                 height={144}
                 alt="New module icon"
-                onClick={() => (document.getElementById('moduleDropdown') as any).showModal()} />
+                onClick={() => (document.getElementById('moduleMenu') as any).showModal()} />
         </div>
-            <dialog id="moduleDropdown" className="modal">
-                <div className="modal-box bg-slate-900 h-96 align-middle items-center">
-                    <h3 className="font-bold text-lg">Select module</h3>
-                    <div className='dropdown dropdown-open '>
-                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Clock</a></li>
-                            <li><a>Tracker</a></li>
-                            <li><a>GitHub</a></li>
+            <dialog id="moduleMenu" className="modal">
+                <div className="modal-box h-96 rounded-md border-green-400 border-2 overflow-hidden">
+                    <h3 className="font-bold text-lg text-center mt-2 mb-2 text-green-400">Select module</h3>
+                    <div className='w-full h-full'>
+                        <ul className="menu rounded-box w-full h-full">
+                            {modules.map((module, index) => (
+                                <li key={index} className=''>
+                                    <a className='w-full place-content-center hover:text-green-400'>{module}</a>
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>
